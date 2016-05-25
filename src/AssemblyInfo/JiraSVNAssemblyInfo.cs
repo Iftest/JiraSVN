c23d0a1513f056aec07fe6c5cc6c7a72ac1d9331
Guy Moore <gmoore@vestmark.com>
2016-05-25 13:41:56 -0400

    upping version of .NET from 3.5 to 4

--- src/AssemblyInfo/JiraSVNAssemblyInfo.cs
+++ src/AssemblyInfo/JiraSVNAssemblyInfo.cs
@@ -14,7 +14,7 @@
 #endregion
 using System.Reflection;
 
-[assembly: AssemblyProduct("https://github.com/dscowan/JiraSVN")]
+[assembly: AssemblyProduct("https://github.com/gmoore777/JiraSVN")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
@@ -25,5 +25,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Roger Knapp")]
 [assembly: AssemblyCopyright("Copyright 2008 by Roger Knapp, Licensed under the Apache License, Version 2.0")]
 
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
